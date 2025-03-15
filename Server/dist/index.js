87db618b89c9a9654f5e22486de0d0d9bc908100
Syed Muhammad Danish <135580119+Danish12387@users.noreply.github.com>
2025-03-16 00:28:02 +0500

    fix: added error middleware

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -8,6 +8,7 @@ import userRoutes from "./routes/user.route.js";
 import jobRoutes from "./routes/job.route.js";
 import postRoutes from "./routes/post.route.js";
 import { Job } from "./models/job.model.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,8 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(errorMiddleware);
+
 const corsOptions = {
     origin: ["http://localhost:3000", "http://localhost:5173", "https://job-portal-alpha-weld.vercel.app/"],
     credentials: true
